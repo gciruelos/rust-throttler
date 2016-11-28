cc826eb1ac284ccdb8b2a842629431836c8bdad7
gciruelos <gonzalo.ciruelos@gmail.com>
2016-11-27 22:33:15 -0300

    simple throttler working

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,20 @@
-mod simple;
-mod blocking;
+pub mod simple;
+
+
+#[cfg(test)]
+mod tests {
+    use simple::SimpleThrottler;
+    use std::time::Duration;
+
+    #[test]
+    fn simple_test() {
+        let mut s = SimpleThrottler::new(1000, Duration::new(1, 0));
+        let mut i = 0;
+
+        while i < 10000 {
+            s.wait();
+            println!("{}", i);
+            i += 1;
+        }
+    }
+}
---
+++ src/simple/mod.rs
@@ -0,0 +1,37 @@
+use std::{thread, time};
+
+pub struct SimpleThrottler {
+    times_: u64,
+    interval_: time::Duration,
+
+    times_left_: u64,
+    last_interval_: time::Instant,
+}
+
+impl SimpleThrottler {
+    pub fn new(times: u64, interval: time::Duration) -> SimpleThrottler {
+        SimpleThrottler {
+            times_: times,
+            interval_: interval,
+
+            times_left_: times,
+            last_interval_: time::Instant::now(),
+        }
+    }
+    pub fn wait(&mut self) {
+        let curr_interval = time::Instant::now()
+            .duration_since(self.last_interval_);
+        if curr_interval > self.interval_ {
+            self.restart();
+        } else if self.times_left_ == 0 {
+            thread::sleep(self.interval_ - curr_interval);
+            self.restart();
+        }
+        self.times_left_ -= 1;
+    }
+    
+    pub fn restart(&mut self) {
+        self.times_left_ = self.times_;
+        self.last_interval_ = time::Instant::now()
+    }
+}
